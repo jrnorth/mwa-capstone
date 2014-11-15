1e1c2576e3e974c71f8bf6484c6418e88f87b0b1
Joseph North <joernorth@gmail.com>
2014-11-15 13:48:53 -0700

    Added Flask-Login extension to the provisioning file

--- eorlive_prototype/provision.sh
+++ eorlive_prototype/provision.sh
@@ -11,6 +11,7 @@ flask/bin/pip install flask
 flask/bin/pip install requests
 flask/bin/pip install Flask-SQLAlchemy
 flask/bin/pip install Flask-Migrate
+flask/bin/pip install Flask-Login
 
 flask/bin/python3.4 -m app.manage db init
 flask/bin/python3.4 -m app.manage db upgrade
