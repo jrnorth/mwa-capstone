5a76df8c1b35c71fb1d9cfdc0d483a40c35a908a
Joseph North <joernorth@gmail.com>
2014-11-18 22:49:45 -0700

    Updated provisioning script with new dependencies needed by pull_chart_data

--- eorlive_prototype/provision.sh
+++ eorlive_prototype/provision.sh
@@ -2,7 +2,7 @@
 echo ---provision.sh---
 
 apt-get update
-apt-get install -y python-virtualenv
+apt-get install -y python-virtualenv python3-dev libpq-dev
 
 cd /mnt/eorlive_prototype
 
@@ -13,6 +13,7 @@ flask/bin/pip install Flask-SQLAlchemy
 flask/bin/pip install Flask-Migrate
 flask/bin/pip install Flask-Login
 flask/bin/pip install requests-futures
+flask/bin/pip install psycopg2
 
 flask/bin/python3.4 -m app.manage db upgrade
 
