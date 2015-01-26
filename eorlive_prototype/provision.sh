0e9a80ca6c88a008db0dafbce132cb07b2177ab9
Joseph North <joernorth@gmail.com>
2015-01-25 21:05:59 -0700

    Updated Vagrant configuration

--- eorlive_prototype/provision.sh
+++ eorlive_prototype/provision.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 echo ---provision.sh---
 
-apt-get update
-apt-get install -y python-virtualenv python3-dev libpq-dev
+#Set a system-wide environment variable for the database's location.
+echo 'export DATABASE_URL="postgres:///postgres"' >> ~/.profile
+
+cat ~/.profile
+
+export DATABASE_URL=postgres:///postgres
+
+sudo apt-get update
+sudo apt-get install -y python-virtualenv python3-dev libpq-dev postgresql
 
 cd /mnt/eorlive_prototype
 
 sudo virtualenv --python=/usr/bin/python3.4 flask
-flask/bin/pip install flask
-flask/bin/pip install requests
-flask/bin/pip install Flask-SQLAlchemy
-flask/bin/pip install Flask-Migrate
-flask/bin/pip install Flask-Login
-flask/bin/pip install requests-futures
-flask/bin/pip install psycopg2
+source flask/bin/activate
+pip install flask requests Flask-SQLAlchemy Flask-Migrate Flask-Login requests-futures psycopg2
 
-flask/bin/python3.4 -m app.manage db upgrade
+sudo -u postgres createuser vagrant
+
+python3.4 -m app.manage db upgrade
 
 crontab crontab
 
