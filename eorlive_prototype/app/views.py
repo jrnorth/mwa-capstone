a6475e863c9e1ba26d57b71e8c2d4b140e459f6f
Joseph North <joernorth@gmail.com>
2015-02-13 13:09:00 -0700

    New observation/error histogram that allows for viewing on variable time scales

--- eorlive_prototype/app/views.py
+++ eorlive_prototype/app/views.py
@@ -79,20 +79,26 @@ def histogram_data():
 	#Wait for the second request to complete, if it hasn't already.
 	end_gps = int(future_end.result().content)
 
+	leap_seconds_result = send_query(g.eor_db, "SELECT leap_seconds FROM leap_seconds ORDER BY leap_seconds DESC LIMIT 1").fetchone()
+
+	leap_seconds = leap_seconds_result[0]
+
+	GPS_LEAP_SECONDS_OFFSET = leap_seconds - 19
+
 	response = send_query(g.eor_db, '''SELECT starttime, obsname
 					FROM mwa_setting
 					WHERE starttime >= {} AND starttime <= {}
 					AND projectid='G0009'
 					ORDER BY starttime ASC'''.format(start_gps, end_gps)).fetchall()
 
-	obscontroller_response = send_query(g.eor_db, '''SELECT ROUND(reference_time) AS reference_time, observation_number, comment
+	obscontroller_response = send_query(g.eor_db, '''SELECT FLOOR(reference_time) AS reference_time
 							FROM obscontroller_log
-							WHERE reference_time >= {} and reference_time <= {}
+							WHERE reference_time >= {} AND reference_time <= {}
 							ORDER BY reference_time ASC'''.format(start_gps, end_gps)).fetchall()
 
-	recvstatuspolice_response = send_query(g.eor_db, '''SELECT ROUND(reference_time) AS reference_time, observation_number, comment
+	recvstatuspolice_response = send_query(g.eor_db, '''SELECT FLOOR(reference_time) AS reference_time
 							FROM recvstatuspolice_log
-							WHERE reference_time >= {} and reference_time <= {}
+							WHERE reference_time >= {} AND reference_time <= {}
 							ORDER BY reference_time ASC'''.format(start_gps, end_gps)).fetchall()
 
 	# The Julian day for January 1, 2000 12:00 UT was 2,451,545 (http://en.wikipedia.org/wiki/Julian_day).
@@ -108,13 +114,15 @@ def histogram_data():
 
 	julian_day_end = delta_end.days + jan_1_2000_julian_day
 
-	observation_counts = [0 for day in range(julian_day_start, julian_day_end + 1)]
+	jan_1_1970 = datetime(1970, 1, 1)
 
-	error_counts = list(observation_counts)
+	jan_6_1980 = datetime(1980, 1, 6)
 
-	obscontroller_error_list = [[] for day in range(julian_day_start, julian_day_end + 1)]
+	GPS_UTC_DELTA = (jan_6_1980 - jan_1_1970).total_seconds()
 
-	recvstatuspolice_error_list = [[] for day in range(julian_day_start, julian_day_end + 1)]
+	observation_counts = []
+
+	error_counts = []
 
 	julian_days = [x for x in range(julian_day_start, julian_day_end + 1)]
 	#Wait for this Web request to complete, if it hasn't already.
@@ -123,6 +131,8 @@ def histogram_data():
 	SECONDS_PER_DAY = 86400
 
 	low_count = high_count = error_count = total_count = 0
+
+	prev_time = 0
 
 	for observation in response:
 		obs_counts_index = 0
@@ -133,7 +143,14 @@ def histogram_data():
 			#start datetime and calculating how many days occurred between it and the observation in question.
 			obs_counts_index = int((observation[0] - julian_start_gps) / SECONDS_PER_DAY)
 
-		observation_counts[obs_counts_index] = observation_counts[obs_counts_index] + 1
+						# Actual UTC time of the observation (for the graph)
+		utc_millis = (observation[0] - GPS_LEAP_SECONDS_OFFSET + GPS_UTC_DELTA) * 1000
+
+		if utc_millis == prev_time:
+			observation_counts[-1][1] += 1
+		else:
+			observation_counts.append([utc_millis, 1])
+			prev_time = utc_millis
 
 		total_count += 1
 
@@ -142,30 +159,72 @@ def histogram_data():
 		elif 'high' in observation[1]:
 			high_count += 1
 
+	prev_time = 0
+
 	for error in obscontroller_response:
-		error_index = int((error[0] - julian_start_gps) / SECONDS_PER_DAY)
-		error_counts[error_index] += 1
+		utc_millis = (error[0] - GPS_LEAP_SECONDS_OFFSET + GPS_UTC_DELTA) * 1000
+		if utc_millis == prev_time:
+			error_counts[-1][1] += 1
+		else:
+			error_counts.append([utc_millis, 1])
+			prev_time = utc_millis
 		error_count += 1
-		obscontroller_error_list[error_index].append([error[0], error[1], error[2]])
+
+	prev_time = 0
 
 	for error in recvstatuspolice_response:
-		error_index = int((error[0] - julian_start_gps) / SECONDS_PER_DAY)
-		error_counts[error_index] += 1
+		utc_millis = (error[0] - GPS_LEAP_SECONDS_OFFSET + GPS_UTC_DELTA) * 1000
+		if utc_millis == prev_time:
+			error_counts[-1][1] += 1
+		else:
+			error_counts.append([utc_millis, 1])
+			prev_time = utc_millis
 		error_count += 1
-		recvstatuspolice_error_list[error_index].append([error[0], error[1], error[2]])
+
+	error_counts.sort(key=lambda error: error[0])
 
 	return render_template('histogram.html', julian_days=julian_days, observation_counts=observation_counts, error_counts=error_counts,
 							total_count=total_count, error_count=error_count, low_count=low_count, high_count=high_count,
-							obscontroller_error_list=obscontroller_error_list, recvstatuspolice_error_list=recvstatuspolice_error_list)
+							range_end=request.form['endtime'])
 
 @app.route('/error_table', methods = ['POST'])
 def error_table():
-	json_object = request.get_json()
-	obscontroller_error_list = json_object['obscontroller_error_list']
-	recvstatuspolice_error_list = json_object['recvstatuspolice_error_list']
-	julian_day = json_object['julian_day']
-	return render_template('error_table.html', obscontroller_error_list=obscontroller_error_list, recvstatuspolice_error_list=recvstatuspolice_error_list,
-							julian_day=julian_day)
+	starttime = datetime.utcfromtimestamp(int(request.form['starttime']) / 1000).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+	endtime = datetime.utcfromtimestamp(int(request.form['endtime']) / 1000).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+	baseUTCToGPSURL = 'http://ngas01.ivec.org/metadata/tconv/?utciso='
+
+	requestURLStart = baseUTCToGPSURL + starttime
+
+	requestURLEnd = baseUTCToGPSURL + endtime
+
+	session = FuturesSession()
+
+	#Start the first Web service request in the background.
+	future_start = session.get(requestURLStart)
+
+	#The second request is started immediately.
+	future_end = session.get(requestURLEnd)
+
+	#Wait for the first request to complete, if it hasn't already.
+	start_gps = int(future_start.result().content)
+
+	#Wait for the second request to complete, if it hasn't already.
+	end_gps = int(future_end.result().content)
+
+	obscontroller_response = send_query(g.eor_db, '''SELECT reference_time, observation_number, comment
+							FROM obscontroller_log
+							WHERE reference_time >= {} AND reference_time < {}
+							ORDER BY reference_time ASC'''.format(start_gps, end_gps)).fetchall()
+
+	recvstatuspolice_response = send_query(g.eor_db, '''SELECT reference_time, observation_number, comment
+							FROM recvstatuspolice_log
+							WHERE reference_time >= {} AND reference_time < {}
+							ORDER BY reference_time ASC'''.format(start_gps, end_gps)).fetchall()
+
+	return render_template('error_table.html', obscontroller_error_list=obscontroller_response, recvstatuspolice_error_list=recvstatuspolice_response,
+							start_time=starttime, end_time=endtime)
 
 @app.before_request
 def before_request():
