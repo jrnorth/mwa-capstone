dcb9df0943fd32943cb7bb8a03c839442a8cca4d
Joseph North <joernorth@gmail.com>
2015-04-15 11:53:57 -0700

    Added generic column graph type

---
+++ eorlive_prototype/app/templates/js/column.js
@@ -0,0 +1,56 @@
+{
+    title: {
+        text: "{{data_source_str}}"
+    },
+    chart: {
+        zoomType: "x",
+        events: {
+            selection: function(event) {
+                if (clickDragMode === 'flag') {
+                    event.preventDefault();
+                    flagClickAndDraggedRange(event);
+                }
+            }
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    xAxis: {
+        type: 'datetime'
+    },
+    legend: {
+        enabled: true
+    },
+    plotOptions: {
+        column: {
+            dataGrouping: {
+                approximation: "average",
+                groupPixelWidth: 80,
+                forced: true
+            },
+            pointPlacement: "between",
+            groupPadding: 0.1,
+            pointPadding: 0
+        }
+    },
+    series: [
+    {% if is_set %}
+        {% for series in graph_data %}
+        {
+            type: 'column',
+            name: "{{series}}",
+            data: {{series}}
+        },
+        {% endfor %}
+    {% else %}
+        {% for series in graph_data.h0 %}
+        {
+            type: 'column',
+            name: "{{series}}",
+            data: graph_data.{{series}}_h0
+        },
+        {% endfor %}
+    {% endif %}
+    ]
+}
